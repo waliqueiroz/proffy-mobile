ac3da11a42eb9f5875194975be2199767c064bda
Wali Queiroz <wsantos077@gmail.com>
2020-08-17 00:24:23 -0300

    Layout finalizado

--- src/components/PageHeader/index.tsx
+++ src/components/PageHeader/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Children, ReactNode } from 'react';
 import { View, Image, Text } from 'react-native';
 import { BorderlessButton } from 'react-native-gesture-handler';
 
@@ -8,10 +8,11 @@ import backIcon from '../../assets/images/icons/back.png';
 import { useNavigation } from '@react-navigation/native';
 
 interface PageHeaderProps {
-    title: string
+    title: string,
+    headerRight?: ReactNode
 }
 
-const PageHeader: React.FC<PageHeaderProps> = ({ title }) => {
+const PageHeader: React.FC<PageHeaderProps> = ({ title, headerRight, children }) => {
 
     const { navigate } = useNavigation();
 
@@ -28,7 +29,13 @@ const PageHeader: React.FC<PageHeaderProps> = ({ title }) => {
 
                 <Image source={logoImg} resizeMode="contain" />
             </View>
-            <Text style={styles.title}>{title}</Text>
+            
+            <View style={styles.header}>
+                <Text style={styles.title}>{title}</Text>
+                {headerRight}
+            </View>
+
+            {children}
         </View>
     );
 }
--- src/pages/Favorites/index.tsx
+++ src/pages/Favorites/index.tsx
@@ -1,12 +1,25 @@
 import React from 'react';
 import { View } from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
 
-import PageHeader from '../../components/PageHeader';
 import styles from './styles';
+import PageHeader from '../../components/PageHeader';
+import TeacherItem from '../../components/TeacherItem';
 
 const Favorites: React.FC = () => {
   return <View style={styles.container} >
     <PageHeader title="Meus proffys avoritos" />
+    <ScrollView
+      style={styles.teacherList}
+      contentContainerStyle={{
+        paddingHorizontal: 16,
+        paddingBottom: 16
+      }} >
+      <TeacherItem />
+      <TeacherItem />
+      <TeacherItem />
+      <TeacherItem />
+    </ScrollView>
   </View>
 }
 
