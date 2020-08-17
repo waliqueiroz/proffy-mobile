26bbea70959c4480ceb5d5fafcefd7a04be862e3
Wali Queiroz <wsantos077@gmail.com>
2020-08-17 00:56:59 -0300

    Listagem da api

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -7,30 +7,41 @@ import heartOutlineIcon from '../../assets/images/icons/heart-outline.png';
 import unfavoriteIcon from '../../assets/images/icons/unfavorite.png';
 import whatsappIcon from '../../assets/images/icons/whatsapp.png';
 
-const TeacherItem: React.FC = () => {
+export interface Teacher {
+    id: number;
+    name: string;
+    subject: string;
+    cost: number;
+    avatar: string;
+    whatsapp: string;
+    bio: string;
+}
+interface TeacherItemProps {
+    teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
     return (
         <View style={styles.container}>
             <View style={styles.profile}>
                 <Image
                     style={styles.avatar}
-                    source={{ uri: 'https://github.com/waliqueiroz.png' }}
+                    source={{ uri: teacher.avatar }}
                 />
 
                 <View style={styles.profileInfo}>
-                    <Text style={styles.name}>Wali Queiroz</Text>
-                    <Text style={styles.subject}>Química</Text>
+                    <Text style={styles.name}>{teacher.name}</Text>
+                    <Text style={styles.subject}>{teacher.subject}</Text>
                 </View>
             </View>
             <Text style={styles.bio}>
-                Entusiasta das melhores tecnologias de química avançada.
-                {'\n'}{'\n'}
-                Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências.
+                {teacher.bio}
             </Text>
 
             <View style={styles.footer}>
                 <Text style={styles.price}>
                     Preço/hora {'   '}
-                    <Text style={styles.priceValue}>R$20,00</Text>
+                    <Text style={styles.priceValue}>R${teacher.cost}</Text>
                 </Text>
 
                 <View style={styles.buttonsContainter}>
--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -3,15 +3,36 @@ import { View, Text } from 'react-native';
 import { Feather } from '@expo/vector-icons'
 
 import PageHeader from '../../components/PageHeader';
-import TeacherItem from '../../components/TeacherItem';
+import TeacherItem, { Teacher } from '../../components/TeacherItem';
 import { ScrollView, TextInput, BorderlessButton, RectButton } from 'react-native-gesture-handler';
 import styles from './styles';
+import api from '../../services/api';
 
 const TeacherList: React.FC = () => {
+  const [teachers, setTeachers] = useState([]);
   const [isFiltersVisible, setIsFilterVisible] = useState(false);
+
+  const [subject, setSubject] = useState('');
+  const [week_day, setWeekDay] = useState('');
+  const [time, setTime] = useState('');
 
   function handleToggleFiltersVisible() {
     setIsFilterVisible(!isFiltersVisible);
+  }
+
+  async function handleFiltersSubmit() {
+
+
+    const response = await api.get('classes', {
+      params: {
+        subject,
+        week_day,
+        time
+      }
+    });
+    
+    setIsFilterVisible(false);
+    setTeachers(response.data);
   }
 
   return (
@@ -28,6 +49,8 @@ const TeacherList: React.FC = () => {
           <View style={styles.searchForm}>
             <Text style={styles.label}>Matérias</Text>
             <TextInput
+              value={subject}
+              onChangeText={(text) => setSubject(text)}
               placeholderTextColor="#c1bccc"
               style={styles.input}
               placeholder="Qual a matéria?"
@@ -37,6 +60,8 @@ const TeacherList: React.FC = () => {
               <View style={styles.inputBlock}>
                 <Text style={styles.label}>Dia da semana</Text>
                 <TextInput
+                  value={week_day}
+                  onChangeText={(text) => setWeekDay(text)}
                   placeholderTextColor="#c1bccc"
                   style={styles.input}
                   placeholder="Qual o dia?"
@@ -45,6 +70,8 @@ const TeacherList: React.FC = () => {
               <View style={styles.inputBlock}>
                 <Text style={styles.label}>Horário</Text>
                 <TextInput
+                  value={time}
+                  onChangeText={(text) => setTime(text)}
                   placeholderTextColor="#c1bccc"
                   style={styles.input}
                   placeholder="Qual horário?"
@@ -52,8 +79,8 @@ const TeacherList: React.FC = () => {
               </View>
 
             </View>
-            
-            <RectButton style={styles.submitButton}>
+
+            <RectButton onPress={handleFiltersSubmit} style={styles.submitButton}>
               <Text style={styles.submitButtonText}>Filtrar</Text>
             </RectButton>
           </View>
@@ -65,10 +92,9 @@ const TeacherList: React.FC = () => {
           paddingHorizontal: 16,
           paddingBottom: 16
         }} >
-        <TeacherItem />
-        <TeacherItem />
-        <TeacherItem />
-        <TeacherItem />
+        {teachers.map((teacher: Teacher) => {
+          return (<TeacherItem key={teacher.id} teacher={teacher} />)
+        })}
       </ScrollView>
     </View>
   );
